73b8e4ec71a8be405bbc8c2f05f561326ed91dd6
Marcos Lee <Marcos@Marcoss-MacBook-Pro.local>
2017-03-15 19:49:40 -0700

    add links to bootstrap, JQUERY, css and FireBase

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+document.ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDkhL19QkZA9WzWiXXJO7RXuQCYnw3XhBk",
+    authDomain: "popcornnow-89d40.firebaseapp.com",
+    databaseURL: "https://popcornnow-89d40.firebaseio.com",
+    storageBucket: "popcornnow-89d40.appspot.com",
+    messagingSenderId: "431519072093"
+  };
+  firebase.initializeApp(config);
+
+//start here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
